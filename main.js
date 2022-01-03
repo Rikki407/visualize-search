a283a26245562ebe4c94cde80f1e9c8ef8705205
Rishab <rlamba3@uic.edu>
2022-01-03 11:34:25 -0600

    REMOVED: unnecessary scripts

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import './stylesheets/neumorphic.css';
 import Board from './src/board';
 import { mobileAndTabletCheck } from './src/utility';
 import { registerSW } from 'virtual:pwa-register';
-import './assetlinks.json';
 registerSW();
 
 const rootCSSKeys = [
